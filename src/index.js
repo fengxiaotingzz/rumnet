78c47bb39ddf9098116e34f85207a718790f2ff8
xiaoting feng <fengxiaotingzz@163.com>
2023-04-21 19:46:57 +0800

    部分

---
+++ src/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const program = new commander.Command();
+const path = require('path')
+const package = require('../package.json')
+const args = process.argv.slice(3)
+
+program.command('init').description('init a new project. usage: runt init <projectName>').action(() => {
+    require('./init')(args[0])
+})
+
+program.version(package.version, '-v --version')
+
+program.parse(process.argv)
