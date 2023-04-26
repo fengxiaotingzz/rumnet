d0c2ccd0604d7281d4a31ba887c970758283e5f5
xiaoting feng <fengxiaotingzz@163.com>
2023-04-26 13:43:03 +0800

    feat:创建完项目之后，自动初始化git仓库，并安装依赖

---
+++ REAME.md
@@ -0,0 +1,8 @@
+这是一个拉取umi或next模版项目的脚手架，实现创建之后，立即可用的效果。目前只支持umi。
+
+### Intall
+`npm i  -g rumnet`
+
+### Usage
+创建名称为test的项目：
+`rumnet init test`
--- src/download.js
+++ src/download.js
@@ -1,13 +1,24 @@
 const downloadFunc = require('download-git-repo');
 const ora = require('ora')
+const shell = require('shelljs')
 
 module.exports = ({url, dir, type}) => {
     const oraLoading = ora(`Downloading ${type} template.`)
     oraLoading.start();
     downloadFunc(`github:${url}`, dir, (err)=>{
         if (!err) {
-            oraLoading.text = `Successfully created ${umi} project!`
-            oraLoading.succeed()
+            oraLoading.text = `Successfully created ${type} project!`
+            oraLoading.succeed();
+            const installLoading = ora(`Installing ${type} template.`)
+            installLoading.start();
+            if(shell.exec(`cd ${dir};git init;npm i`).code === 0) {
+                oraLoading.text = `Successfully installed ${type} project!`
+                installLoading.succeed();
+            } else {
+                installLoading.stop();
+            }
+        } else {
+            oraLoading.stop()
         }
     })
 }
